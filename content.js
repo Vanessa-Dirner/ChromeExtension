3f6b266da1ffd62d37acb50b6a7661d73fd1327c
Vanessa-Dirner <66662397+Vanessa-Dirner@users.noreply.github.com>
2020-06-11 16:06:38 -0700

    more tutorials

--- content.js
+++ content.js
@@ -1,1 +1,10 @@
-document.addEventListener('scroll', () => alert('Watch out, it is DJ Khaled!'));
+chrome.runtime.onMessage.addListener(gotMessage);
+function gotMessage(message,sender,sendresponse)
+{
+	console.log(message.txt);
+	let paragraphs = document.getElementsByTagName("p");
+	for(elt of paragraphs)
+	{
+		elt.style['background-color'] = '#00CED1';
+	}
+}
