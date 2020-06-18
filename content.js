1731b59aa186b12129cbde91080dafcfc94d43af
Luis Gascon <luis.gcodes@gmail.com>
2020-06-18 16:18:09 -0700

    Added a background script for testing

--- content.js
+++ content.js
@@ -1,9 +1,15 @@
 /* content.js communicates with the browser page  */
 
-
+console.log("This extension is currently running");
 // Any time the 'Change' button is clicked
 chrome.runtime.onMessage.addListener((req)=>{
    document.querySelectorAll('div').forEach((items)=>{
        items.style.background = req;
    }) ;
 })
+
+// let para = document.getElementsByTagName('p');
+// for(p of para) {
+    // p.style.background = 'blue';
+// }
+    
